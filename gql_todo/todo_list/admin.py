ef2003a217a97673a8ded931d0ed2b3de9191398
Xavier Lefèvre <xavier.lefevre@me.com>
2017-12-10 20:26:41 +0100

    init(-): add list and item to admins

--- gql_todo/todo_list/admin.py
+++ gql_todo/todo_list/admin.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import List, Item
+
+admin.site.register(List)
+admin.site.register(Item)
