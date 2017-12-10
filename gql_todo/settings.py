4498e5309578d7cd968d55496a886a554cf9e6c6
Xavier Lefèvre <xavier.lefevre@me.com>
2017-12-10 19:57:08 +0100

    init(-): run first migration

--- gql_todo/settings.py
+++ gql_todo/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gql_todo/todo',
+    'gql_todo.todo',
 ]
 
 MIDDLEWARE = [
