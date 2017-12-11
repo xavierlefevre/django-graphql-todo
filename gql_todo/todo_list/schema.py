085f99e34824de8a85cd65bb66542dac29264a67
Xavier Lefèvre <xavier.lefevre@me.com>
2017-12-11 20:10:25 +0100

    feat(-): add filters

--- gql_todo/todo_list/schema.py
+++ gql_todo/todo_list/schema.py
@@ -1,5 +1,6 @@
 from graphene import relay, AbstractType
 from graphene_django import DjangoConnectionField, DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 
 from gql_todo.todo_list.models import Item, List
 
@@ -7,19 +8,25 @@ from gql_todo.todo_list.models import Item, List
 class ItemNode(DjangoObjectType):
     class Meta:
         model = Item
+        filter_fields = {
+            'text': ['exact', 'icontains', 'istartswith'],
+        }
         interfaces = (relay.Node, )
 
 
 class ListNode(DjangoObjectType):
     class Meta:
         model = List
+        filter_fields = {
+            'title': ['exact', 'icontains', 'istartswith'],
+        }
         interfaces = (relay.Node, )
 
 
 class Query(AbstractType):
     item = relay.Node.Field(ItemNode)
-    items = DjangoConnectionField(
+    items = DjangoFilterConnectionField(
         ItemNode, description='An item of the Todo list')
     list = relay.Node.Field(ListNode)
-    lists = DjangoConnectionField(
+    lists = DjangoFilterConnectionField(
         ListNode, description='A list')
