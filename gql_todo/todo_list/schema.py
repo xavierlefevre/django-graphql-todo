a2b4027517400ef10004ebb1b0b8f14754079591
Xavier Lefèvre <xavier.lefevre@me.com>
2017-12-11 20:00:00 +0100

    feat(-): can query relations

--- gql_todo/todo_list/schema.py
+++ gql_todo/todo_list/schema.py
@@ -1,7 +1,7 @@
 from graphene import relay, AbstractType
 from graphene_django import DjangoConnectionField, DjangoObjectType
 
-from gql_todo.todo_list.models import Item
+from gql_todo.todo_list.models import Item, List
 
 
 class ItemNode(DjangoObjectType):
@@ -10,6 +10,16 @@ class ItemNode(DjangoObjectType):
         interfaces = (relay.Node, )
 
 
+class ListNode(DjangoObjectType):
+    class Meta:
+        model = List
+        interfaces = (relay.Node, )
+
+
 class Query(AbstractType):
+    item = relay.Node.Field(ItemNode)
     items = DjangoConnectionField(
         ItemNode, description='An item of the Todo list')
+    list = relay.Node.Field(ListNode)
+    lists = DjangoConnectionField(
+        ListNode, description='A list')
